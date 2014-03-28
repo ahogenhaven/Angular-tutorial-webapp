56f09ebe8b3b5bb97e30479283ac66adb3598149
ahogenhaven <andreashogen@gmail.com>
2014-03-28 16:58:21 +0100

    qwe

--- app/js/controllers/EditEventController.js
+++ app/js/controllers/EditEventController.js
@@ -2,7 +2,17 @@
 
 eventsApp.controller('EditEventController',
 	function EditEventController($scope) {
+		$scope.saveEvent = function(event, newEventForm) {
+			console.log(newEventForm);
+			window.alert ('event ' + event.name + ' saved!');
 
+		}
+
+
+		$scope.cancelEdit = function () {
+			window.location = "EventDetails.html"
+
+		}
 	}
 );
 
